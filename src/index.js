1ea91aa58e5119780c1476bbde711e8e5b0d651e
CleidyBelizario <you@example.com>
2021-03-24 12:10:55 -0500

    Proyecto terminado al trabajar el DOM

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import validator from './validator.js';
 
 console.log(validator);
 
-var despensa = document.getElementById("despensa");
-var aseo = document.getElementById("aseo");
-var lacteos = document.getElementById("lacteos");
+let despensa = document.getElementById("despensa");
+let aseo = document.getElementById("aseo");
+let lacteos = document.getElementById("lacteos");
 //para cambiar al formulario 
 function comprarT() {
   document.getElementById("categorias").classList.add("oculta");
@@ -17,32 +17,38 @@ aseo.addEventListener("click", comprarT, false);
 lacteos.addEventListener("click", comprarT, false);
 
 // para validar la tarjeta con lo que ingrese el cliente
-var numeroIngresado = document.getElementById("numeroTarjeta");
+let numeroIngresado = document.getElementById("numeroTarjeta");
 
 // funcion para que el cliente ingrese el numero de tarjeta
 function obtenerNumeroIngresado() {
   document.getElementById("contenedor_formulario").classList.add("oculta");
   document.getElementById("botonValidarT").remove("oculta");
-  var Nombre = document.getElementById("nombre").value;
-  var mostrarNombre = Nombre
-  var gracias = "¡Gracias por tu compra!, ";
-  var nombre1 = document.getElementById("name").innerHTML = mostrarNombre;
-  var saludoFinal = gracias.concat(nombre1);
-  document.getElementById("name").innerHTML = saludoFinal
+  
   return numeroIngresado.value;
 }
 
 document.getElementById("botonValidarT").addEventListener("click",
   function resultado() {
 
-    var numeroTC = obtenerNumeroIngresado();
-    var tarjetaValida = validator.isValid(numeroTC);
-    var resultado = document.getElementById("resultadoTarjeta")
+  let numeroTC = obtenerNumeroIngresado();
+  let tarjetaValida = validator.isValid(numeroTC);
+  let resultado = document.getElementById("resultadoTarjeta")
+  let maski = validator.maskify(numeroTC)
     if (validator.isValid(numeroTC) === true) {
-      confirm("tarjeta valida")
+    // confirm("tarjeta valida")
+  let tarjetaV = document.getElementById("numeroTarjeta").value;
+  let mostrartarjeta = tarjetaV.maski
+  let gracias1 = " ¡Gracias por tu compra! Tarjeta Valida" 
+  //let nombre2 = document.getElementById("numeroTarjeta").innerHTML = mostrartarjeta;
+  let saludoFinal1 = gracias1 + maski;
+  document.getElementById("name").innerHTML = saludoFinal1 
     }
     else {
-      confirm("tarjeta invalida ")
+      let tarjetaV2 = document.getElementById("numeroTarjeta").value;
+      let mostrartarjeta2 = tarjetaV2
+      let gracias2 = "Tarjeta Invalida"  
+      let saludoFinal2 =  gracias2 + maski;
+      document.getElementById("name").innerHTML = saludoFinal2
 
     }
   });
